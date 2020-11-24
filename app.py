e51aa16ede06edab66f7e888bdc3b07c93676988
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-24 15:01:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ if uploaded_file is not None:
      bytes_data = uploaded_file.read()
      st.write(bytes_data)
 
-    # Load data example (dari functional maupun nonfunctional)
-    statement = bytes_data
+     # Load data example (dari functional maupun nonfunctional)
+     statement = bytes_data
 
-    # Get text to clean (dari row yang diinginkan)
-    text_to_clean = list(statement['Requirement Statement'])
+     # Get text to clean (dari row yang diinginkan)
+     text_to_clean = list(statement['Requirement Statement'])
 
-    # Clean text
-    print("Loading Original & Cleaned Text...")
-    cleaned_text = apply_cleaning(text_to_clean)
+     # Clean text
+     print("Loading Original & Cleaned Text...")
+     cleaned_text = apply_cleaning(text_to_clean)
 
-    # Show first example
-    text_df = pd.DataFrame([text_to_clean, cleaned_text],index=['ORIGINAL','CLEANED'], columns= statement['ID'])
-    st.write(text_df)
+     # Show first example
+     text_df = pd.DataFrame([text_to_clean, cleaned_text],index=['ORIGINAL','CLEANED'], columns= statement['ID'])
+     st.write(text_df)
