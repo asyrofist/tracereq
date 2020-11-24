a686af2be723517bfc3aef73b4bf25812af0674e
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-24 16:36:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -98,14 +98,9 @@ if index0 is not None:
           XVSM_scaled = scaler.fit_transform(klaster_df)
           st.write(XVSM_scaled)
           
-          correct = 0
-          for i in range(len(klaster_df)):
-              predict_me = np.array(XVSM_scaled[i].astype(float))
-              predict_me = predict_me.reshape(-1, len(predict_me))
-              prediction = kmeans.predict(predict_me)
-              if prediction[0] == klaster_df[i].all():
-                  correct += 1
-          st.write(correct/len(XVSM_scaled))
+          actual = cosine_similaritas
+          resultsaf = classification_report(actual, XVSM_scaled)
+
 
      elif genre == 'Ontology':
           st.write("ontology.")
