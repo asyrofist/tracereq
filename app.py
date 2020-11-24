f0023e5d2fc76cb1a65355ad235022d353a6bdcb
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-24 17:46:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,6 @@ if index0 is not None:
             sentences = [word_tokenize(num) for num in cleaned_text]
             for i in range(len(sentences)):
                  sentences[i] = TaggedDocument(words = sentences[i], tags = ['sent{}'.format(i)])    # converting each sentence into a TaggedDocument
-            st.write(sentences)
             st.sidebar.subheader("Model Parameter")
             size_value = st.sidebar.slider('Berapa Size Model?', 0, 200, len(doc_feature))
             iterasi_value = st.sidebar.slider('Berapa Iterasi Model?', 0, 100, 10)
@@ -152,6 +151,8 @@ if index0 is not None:
             model = Doc2Vec(documents = sentences, dm = dimension_value, size = size_value, window = window_value, min_count = 1, iter = iterasi_value, workers = Pool()._processes)
             model.init_sims(replace = True)
 #             nilai_vektor = [model.infer_vector("sent{}".format(num)) for num in range(0, len(sentences))]
+            nilai_vektor = [model.infer_vector(sentences)]
+          
 #             id_requirement = fulldataset(index0, index1)['ID']
 #             df_vektor = pd.DataFrame(nilai_vektor, index=id_requirement, columns= ['vektor {}'.format(num) for num in range(0, size_value)])
 #             st.dataframe(df_vektor)
