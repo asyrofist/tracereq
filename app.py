c02709f74768d62292498987a6e65bf890677284
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-24 17:47:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -152,8 +152,6 @@ if index0 is not None:
             model = Doc2Vec(documents = sentences, dm = dimension_value, size = size_value, window = window_value, min_count = 1, iter = iterasi_value, workers = Pool()._processes)
             model.init_sims(replace = True)
 #             nilai_vektor = [model.infer_vector("sent{}".format(num)) for num in range(0, len(sentences))]
-            nilai_vektor = [model.infer_vector(sentences)]
-          
 #             id_requirement = fulldataset(index0, index1)['ID']
 #             df_vektor = pd.DataFrame(nilai_vektor, index=id_requirement, columns= ['vektor {}'.format(num) for num in range(0, size_value)])
 #             st.dataframe(df_vektor)
